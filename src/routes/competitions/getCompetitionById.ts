325aad6e14a3f88f5d9f3f6218583ed09ede4a6a
Vincent Ferraro <v.ferraro@live.fr>
2023-06-23 21:36:21 +0200

    add getCompetitionById

--- src/routes/competitions/getCompetitionById.ts
+++ src/routes/competitions/getCompetitionById.ts
@@ -1,16 +1,19 @@
 import { Competition } from "../../db/models/competition";
 import { Request, Response, NextFunction } from "express";
 
-export async function getCompetitions(
+export async function getCompetitionById(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
   try {
-    const competition = await Competition.findAll();
-    console.log(competition);
+    const id: number = parseInt(req.params.id);
+    if (!id) {
+      throw new Error("NO ID");
+    }
+    const competition = (await Competition.findByPk(id)) as Competition;
     res.status(200).json(competition);
   } catch (err) {
-    res.status(400).send("ERROR");
+    next(err);
   }
 }
--- src/routes/competitions/index.ts
+++ src/routes/competitions/index.ts
@@ -1,10 +1,11 @@
 import { addCompetition } from "./addCompetitions";
 import { getCompetitions } from "./getCompetitions";
-import { app } from "../../../app";
+import { getCompetitionById } from "./getCompetitionById";
 import { Router } from "express";
 // COMPETITION ROUTES
 const router: Router = Router();
 router.post("/", addCompetition);
 router.get("/", getCompetitions);
+router.get("/:id", getCompetitionById);
 
 export default router;
